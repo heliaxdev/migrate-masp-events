a109336c747ddd311bd047587161c668bb9bd9c3
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-03-25 15:11:07 +0000

    skip empty prints and fmt as json

--- print.go
+++ print.go
@@ -1,27 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
+	"slices"
+	"strconv"
 
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
 
+	"github.com/cometbft/cometbft/abci/types"
 	cmtstate "github.com/cometbft/cometbft/proto/tendermint/state"
 )
 
 type argsPrint struct {
+	SkipEmpty bool
 	CometHome string
 }
 
 func RegisterCommandPrint(subCommands map[string]*SubCommand) {
 	subCommands["print"] = &SubCommand{
 		Args:        &argsPrint{},
-		Description: "print all end blocks instances in the state db of cometbft",
+		Description: "print all end blocks events in the state db of cometbft",
 		ConfigureFlags: func(iArgs any, flags *flag.FlagSet) {
 			args := iArgs.(*argsPrint)
+
 			flags.StringVar(&args.CometHome, "cometbft-homedir", "", "path to cometbft dir (e.g. .namada/namada.5f5de2dd1b88cba30586420/cometbft)")
+			flags.BoolVar(&args.SkipEmpty, "skip-empty", false, "skip printing blocks with no events")
 		},
 		Entrypoint: func(iArgs any) error {
 			args := iArgs.(*argsPrint)
@@ -32,14 +41,16 @@ func RegisterCommandPrint(subCommands map[string]*SubCommand) {
 			}
 			defer db.Close()
 
-			return printEndBlocks(db)
+			return printEndBlocks(db, args.SkipEmpty)
 		},
 	}
 }
 
-func printEndBlocks(db *leveldb.DB) error {
+func printEndBlocks(db *leveldb.DB, skipEmpty bool) error {
 	iter := db.NewIterator(util.BytesPrefix([]byte("abciResponsesKey:")), &opt.ReadOptions{})
 	defer iter.Release()
+
+	var blockEvents []block
 
 	for iter.Next() {
 		abciResponses := new(cmtstate.ABCIResponses)
@@ -47,9 +58,51 @@ func printEndBlocks(db *leveldb.DB) error {
 		if err != nil {
 			return fmt.Errorf("failed to unmarshal abci responses from state db: %w", err)
 		}
-		fmt.Printf("Key = %q\n", string(iter.Key()))
-		fmt.Printf("Value = %#v\n", abciResponses.EndBlock)
+
+		if skipEmpty && abciResponses.EndBlock.Events == nil {
+			continue
+		}
+
+		height, err := extractHeight(iter.Key())
+		if err != nil {
+			return err
+		}
+
+		blockEvents = append(blockEvents, block{
+			BlockHeight: height,
+			Events:      abciResponses.EndBlock.Events,
+		})
+	}
+
+	// NB: sort data by block height
+	slices.SortFunc(blockEvents, func(b1, b2 block) int {
+		return b1.BlockHeight - b2.BlockHeight
+	})
+
+	writer := bufio.NewWriter(os.Stdout)
+	encoder := json.NewEncoder(writer)
+
+	err := encoder.Encode(blockEvents)
+	if err != nil {
+		return fmt.Errorf("failed to encode block events as json: %w", err)
 	}
 
 	return nil
+}
+
+func extractHeight(key []byte) (int, error) {
+	// NB: skip "abciResponsesKey:"
+	heightStr := string(key[17:])
+
+	h, err := strconv.Atoi(heightStr)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse block height from db key: %w", err)
+	}
+
+	return h, nil
+}
+
+type block struct {
+	BlockHeight int           `json:"block_height"`
+	Events      []types.Event `json:"events"`
 }
