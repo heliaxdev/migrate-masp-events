301c4b8286a3d1a0de0261929a5c7c9795bfdc02
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-03-25 14:13:46 +0000

    migrate subcmd

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"github.com/syndtr/goleveldb/leveldb/util"
-
-	cmtstate "github.com/cometbft/cometbft/proto/tendermint/state"
-)
-
 // TODO:
 //
 // - include backup/restore functionality to dump old block events
@@ -18,42 +10,11 @@ import (
 //   `masp/fee-payment` events for this program to be indempotent
 
 func main() {
-	db, err := leveldb.OpenFile("/Users/sugo/Code/heliax/namada/.namada/validator-0/local.49606e92a35a899e7e2559ef/cometbft/data/state.db", nil)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	subCommands := make(map[string]*SubCommand)
 
-	migrateEvents(db)
-	printEndBlocks(db)
-}
+	RegisterCommandPrint(subCommands)
+	RegisterCommandMigrate(subCommands)
 
-func migrateEvents(db *leveldb.DB) {
-	iter := db.NewIterator(util.BytesPrefix([]byte("abciResponsesKey:")), &opt.ReadOptions{})
-	defer iter.Release()
-
-	updates := make(map[string][]byte)
-
-	for iter.Next() {
-		abciResponses := new(cmtstate.ABCIResponses)
-		err := abciResponses.Unmarshal(iter.Value())
-		if err != nil {
-			panic(err)
-		}
-		for i := 0; i < len(abciResponses.EndBlock.Events); i++ {
-			abciResponses.EndBlock.Events[i].Type = "asdjaslkdjlkasjdlkjadlj"
-		}
-		value, err := abciResponses.Marshal()
-		if err != nil {
-			panic(err)
-		}
-		updates[string(iter.Key())] = value
-	}
-
-	for key, value := range updates {
-		err := db.Put([]byte(key), value, new(opt.WriteOptions))
-		if err != nil {
-			panic(err)
-		}
-	}
+	r := NewRunner(subCommands)
+	r.Run()
 }
---
+++ migrate.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+
+	cmtstate "github.com/cometbft/cometbft/proto/tendermint/state"
+)
+
+type argsMigrate struct {
+	CometHome string
+}
+
+func RegisterCommandMigrate(subCommands map[string]*SubCommand) {
+	subCommands["migrate"] = &SubCommand{
+		Args:        &argsMigrate{},
+		Description: "migrate old masp events (<= namada v1.1.4) in the state db of cometbft",
+		ConfigureFlags: func(iArgs any, flags *flag.FlagSet) {
+			args := iArgs.(*argsMigrate)
+			flags.StringVar(&args.CometHome, "cometbft-homedir", "", "path to cometbft dir (e.g. .namada/namada.5f5de2dd1b88cba30586420/cometbft)")
+		},
+		Entrypoint: func(iArgs any) error {
+			args := iArgs.(*argsMigrate)
+
+			if args.CometHome == "" {
+				return fmt.Errorf("no cometbft home dir provided as arg")
+			}
+
+			dbPath := filepath.Join(args.CometHome, "data", "state.db")
+
+			db, err := leveldb.OpenFile(dbPath, nil)
+			if err != nil {
+				return fmt.Errorf("failed to open db in %s: %w", dbPath, err)
+			}
+			defer db.Close()
+
+			return migrateEvents(db)
+		},
+	}
+}
+
+func migrateEvents(db *leveldb.DB) error {
+	iter := db.NewIterator(util.BytesPrefix([]byte("abciResponsesKey:")), &opt.ReadOptions{})
+	defer iter.Release()
+
+	updates := make(map[string][]byte)
+
+	for iter.Next() {
+		abciResponses := new(cmtstate.ABCIResponses)
+		err := abciResponses.Unmarshal(iter.Value())
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal abci responses from state db: %w", err)
+		}
+		for i := 0; i < len(abciResponses.EndBlock.Events); i++ {
+			abciResponses.EndBlock.Events[i].Type = "asdjaslkdjlkasjdlkjadlj"
+		}
+		value, err := abciResponses.Marshal()
+		if err != nil {
+			return fmt.Errorf("failed to marshal abci responses: %w", err)
+		}
+		updates[string(iter.Key())] = value
+	}
+
+	for key, value := range updates {
+		err := db.Put([]byte(key), value, new(opt.WriteOptions))
+		if err != nil {
+			return fmt.Errorf("failed to write migrated abci responses to db: %w", err)
+		}
+	}
+
+	return nil
+}
