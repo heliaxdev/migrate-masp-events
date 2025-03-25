ced262f0a498758a56bcbef5236db48b848417d9
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-03-25 10:54:02 +0000

    todos

--- main.go
+++ main.go
@@ -10,6 +10,15 @@ import (
 	cmtstate "github.com/cometbft/cometbft/proto/tendermint/state"
 )
 
+// TODO:
+//
+// - include backup/restore functionality to dump old block events
+//   and load them again if something goes wrong
+// - hardcode phase3 start height, to only process the events in
+//   those blocks and speed up the migration
+// - check if end block events already contain `masp/transfer` or
+//   `masp/fee-payment` events for this program to be indempotent
+
 func main() {
 	db, err := leveldb.OpenFile("/Users/sugo/Code/heliax/namada/.namada/validator-0/local.49606e92a35a899e7e2559ef/cometbft/data/state.db", nil)
 	if err != nil {
