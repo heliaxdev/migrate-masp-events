b8d4934170421543c634c0357ba4b48b9796aea3
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-03-27 13:24:55 +0000

    add compact size tests

---
+++ tx/compact_size_test.go
@@ -0,0 +1,36 @@
+package tx
+
+import (
+	"testing"
+
+	"github.com/gagliardetto/binary"
+)
+
+func TestCompactSize(t *testing.T) {
+	eval := func(expected uint64, data []byte) {
+		var c CompactSize
+
+		d := bin.NewBorshDecoder(data)
+
+		if err := d.Decode(&c); err != nil {
+			t.Fatalf("failed to decode %v: %s", data, err)
+		}
+
+		if c.Size != expected {
+			t.Fatalf("failed to decode %v: expected %d, got %d", data, expected, c.Size)
+		}
+	}
+
+	eval(0, []byte{0})
+	eval(1, []byte{1})
+	eval(252, []byte{252})
+	eval(253, []byte{253, 253, 0})
+	eval(254, []byte{253, 254, 0})
+	eval(255, []byte{253, 255, 0})
+	eval(256, []byte{253, 0, 1})
+	eval(256, []byte{253, 0, 1})
+	eval(65535, []byte{253, 255, 255})
+	eval(65536, []byte{254, 0, 0, 1, 0})
+	eval(65537, []byte{254, 1, 0, 1, 0})
+	eval(33554432, []byte{254, 0, 0, 0, 2})
+}
