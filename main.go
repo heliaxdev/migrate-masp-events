5aa6e91d4f9122f965c49d01d5bf59b3ffe3740a
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-03-28 15:02:27 +0000

    rm todo

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-// TODO:
-//
-// - include backup/restore functionality to dump old block events
-//   and load them again if something goes wrong
-// - hardcode phase3 start height, to only process the events in
-//   those blocks and speed up the migration
-
 func main() {
 	subCommands := make(map[string]*SubCommand)
 
